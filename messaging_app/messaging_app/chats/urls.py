749c8f0d0b9544ed1401f51231b9ca22790bf62f
kedistde <76200367+kedistde@users.noreply.github.com>
2025-09-30 15:53:08 +0300

    Update urls.py

--- messaging_app/messaging_app/chats/urls.py
+++ messaging_app/messaging_app/chats/urls.py
@@ -8,6 +8,18 @@ from rest_framework_simplejwt.views import (
 
 from chats.views import ConversationViewSet, MessageViewSet
 
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, ConversationViewSet, MessageViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'conversations', ConversationViewSet)
+router.register(r'messages', MessageViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
